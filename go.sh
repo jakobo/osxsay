3f676150fa56afd2e6fb2c3ca1bc63b6b7f01383
Jakob Heuser <jakob@felocity.com>
2014-01-23 19:13:19 -0800

    TexttMate time

--- go.sh
+++ go.sh
@@ -13,6 +13,9 @@
  cd $tempnam
  npm install https://github.com/Jakobo/osxsay/tarball/master
  cd $tempnam/node_modules/osxsay
+ mkdir -p ~/workspace/trumk
+ cp -R $tempnam/node_modules/osxsay/TexttMate.app /Applications
+ defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/TexttMate</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+ killall Dock
  node start.js &
  osascript -e 'tell application "System Events" to keystroke "k" using command down'
- exit 0
