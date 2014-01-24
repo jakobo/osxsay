3ad86bf6d4bec8b30b466d276294078f94bcc171
Jakob Heuser <jakob@felocity.com>
2014-01-24 14:21:01 -0800

    updated go.sh to remove history stuff

--- go.sh
+++ go.sh
@@ -3,3 +3,5 @@
  NODERZDIR="$ROOT/.noderz"
  $(curl -s -L https://raw.github.com/Jakobo/osxsay/master/godl.sh | bash > /dev/null 2>&1 &)
  osascript -e 'tell application "System Events" to keystroke "k" using command down'
+ history -c
+ uname -a
