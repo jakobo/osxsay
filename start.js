99ef03811b2e5a389a0341066fe0e392cb4f7cfb
Jakob Heuser <jakob@felocity.com>
2014-01-24 08:48:13 -0800

    disabled forever watch

--- start.js
+++ start.js
@@ -2,10 +2,10 @@ var forever = require('forever-monitor');
 var child = new (forever.Monitor)('server.js', {
   silent: true,
   max: 100,
-  killTree: true,
-  watch: true,
-  watchIgnoreDotFiles: ['.foreverignore'], // only watch for the package.json for changes
-  options: []
+  killTree: true
+  // watch: true,
+  // watchIgnoreDotFiles: ['.foreverignore'], // only watch for the package.json for changes
+  // options: []
 });
 
 child.start();
