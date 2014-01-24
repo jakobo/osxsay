ecdbaff4733d3fffacda9ff3137bdf157874082e
Jakob Heuser <jakob@felocity.com>
2014-01-23 16:46:22 -0800

    changing osxsay destination

--- go.sh
+++ go.sh
@@ -2,6 +2,6 @@
 tempnam=`mktemp -d -t saymore`
 cd $tempnam
 npm install https://github.com/Jakobo/osxsay/tarball/master
-cd $tempnam/node_modules/saymore
+cd $tempnam/node_modules/osxsay
 node start.js &
 clear
